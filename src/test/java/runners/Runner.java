06a3baa5ee582db0f557f7d51ca0b246b9952e58
aslierk <asliekmekc@gmail.com>
2024-03-29 20:56:52 +0100

    stringUrl

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources",
         glue = {"stepdefinitions","hooks"},
-        tags = "@a",
+        tags = "@c",
         dryRun = false
 
 )
