c9078ce0d067be420e11996b3760c4875bc5139a
yukselnevfel <nyuksel3410@gmail.com>
2024-03-29 21:11:33 +0300

    güncel

--- src/test/java/stepdefinitions/VisitorHomePageStepDef.java
+++ src/test/java/stepdefinitions/VisitorHomePageStepDef.java
@@ -1,8 +1,5 @@
 package stepdefinitions;
 import io.cucumber.java.en.Given;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 import pages.Base;
 import utils.Driver;
 import utils.ConfigReader;
@@ -10,60 +7,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class VisitorHomePageStepDef extends Base {
-
-    //-------------Login Steps AYCA-----------------//
-    @Given("Open browser and Go to {string}")
-    public void open_browser_and_go_to(String string) {
-
-
-
-    }
-    @Given("Close the pop-up message")
-    public void close_the_pop_up_message() {
-
-
-
-    }
-    @Given("Verify that the {string} is visible")
-    public void verify_that_the_is_visible(String string) {
-
-
-
-
-    }
-    @Given("Click on the {string}")
-    public void click_on_the(String string) {
-
-
-
-
-    }
-    @Given("Verify that navigated to the {string}")
-    public void verify_that_navigated_to_the(String string) {
-
-
-
-    }
-    @Given("Verify that the relevant image")
-    public void verify_that_the_relevant_image() {
-
-
-
-    }
-    @Given("Verify that the {string} is clickable")
-    public void verify_that_the_is_clickable(String string) {
-
-
-
-    }
-    @Given("Verify that the {string} is selectable")
-    public void verify_that_the_is_selectable(String string) {
-
-
-
-    }
-
-
 
 
     
@@ -82,11 +25,11 @@ public class VisitorHomePageStepDef extends Base {
     }
 
     @Given("Verify that the {string} is visible")
-    public void verify_that_the_is_visible() {assertTrue(visitorHomePage.linkHeaderBlog.isDisplayed());
+    public void verify_that_the_is_visible(String string) {assertTrue(visitorHomePage.linkHeaderBlog.isDisplayed());
     }
 
     @Given("Verify that the {string} is active")
-    public void verify_that_the_is_active() {
+    public void verify_that_the_is_active(String string) {
         assertTrue(visitorHomePage.linkHeaderBlog.isEnabled());
     }
 
---
+++ src/test/resources/visitor/US_003.feature
@@ -0,0 +1,6 @@
+@a
+Feature: There should be an informative blog page on the site
+
+  Background:
+    * Open browser and Go to "URL"
+    * Close the pop-up message
