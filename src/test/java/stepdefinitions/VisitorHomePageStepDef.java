451ad0e241c04819611e597e60eb61ce315c1d80
EsraAdileYilmaz <yilmazesraadile@gmail.com>
2024-03-30 12:23:06 +0100

    update/class/visitorstep/us02/30/03/2024

--- src/test/java/stepdefinitions/VisitorHomePageStepDef.java
+++ src/test/java/stepdefinitions/VisitorHomePageStepDef.java
@@ -14,10 +14,7 @@ public class VisitorHomePageStepDef extends Base {
     //-------------Login Steps AYCA-----------------//
 
 
-
-
-    
-     //-------------Login Steps FIKRET-----------------/
+    //-------------Login Steps FIKRET-----------------/
 
     @Given("Open browser and Go to {string}")
     public void open_browser_and_go_to(String bSCUrl) {
@@ -32,7 +29,8 @@ public class VisitorHomePageStepDef extends Base {
     }
 
     @Given("Verify that the Blog link is visible")
-    public void verify_that_the_is_visible() {assertTrue(visitorHomePage.linkHeaderBlog.isDisplayed());
+    public void verify_that_the_is_visible() {
+        assertTrue(visitorHomePage.linkHeaderBlog.isDisplayed());
     }
 
     @Given("Verify that the Blog link is active")
@@ -47,8 +45,9 @@ public class VisitorHomePageStepDef extends Base {
 
     @Given("Verify that navigated to the Blog Page")
     public void verify_that_navigated_to_the_page() {
-           assertEquals("https://qa.buysellcycle.com/blog", Driver.getDriver().getCurrentUrl());
+        assertEquals("https://qa.buysellcycle.com/blog", Driver.getDriver().getCurrentUrl());
     }
+
     @Given("Verify that navigated to the Read More Page")
     public void verify_that_navigated_to_the_read_more_page() {
         clickWithJS(visitorHomePage.linkReadMore);
@@ -57,17 +56,19 @@ public class VisitorHomePageStepDef extends Base {
 
     @Given("Enter a keyword into the search texBox and press Enter")
     public void enter_a_keyword_into_the_tex_box_and_press() {
-        visitorHomePage.blogSearchBox.sendKeys("metaverse "+ Keys.ENTER);
+        visitorHomePage.blogSearchBox.sendKeys("metaverse " + Keys.ENTER);
     }
 
     @Given("Verify that the search results are relevant to the entered keyword")
     public void verify_that_the_search_results_are_relevant_to_the_entered_keyword() {
         assertTrue(visitorHomePage.labelMetaverseText.getText().contains("Metaverse"));
     }
+
     @When("Select a category from the Category section")
     public void select_a_category_from_the_category_section() {
         clickWithJS(visitorHomePage.selectCategoryPost);
     }
+
     @Then("Verify that blogs related to the selected category are listed")
     public void verify_that_blogs_related_to_the_selected_category_are_listed() {
         assertEquals("https://qa.buysellcycle.com/blog/category/posts/e-commerce-7", Driver.getDriver().getCurrentUrl());
@@ -77,6 +78,7 @@ public class VisitorHomePageStepDef extends Base {
     public void select_a_keyword_from_the_keyword_section() {
         visitorHomePage.blogKeyword.sendKeys("shopping");
     }
+
     @Then("Verify that relevant blogs are listed based on the selected keyword")
     public void verify_that_relevant_blogs_are_listed_based_on_the_selected_keyword() {
         String actualTagName = visitorHomePage.blogKeyword.getTagName();
@@ -88,6 +90,7 @@ public class VisitorHomePageStepDef extends Base {
     public void select_a_category_from_the_popular_posts_section() {
         clickWithJS(visitorHomePage.popularCategoryBlog);
     }
+
     @Then("Verify that the page with the selected blog post has been accessed")
     public void verify_that_the_page_with_the_selected_blog_post_has_been_accessed() {
         assertTrue(Driver.getDriver().getCurrentUrl().startsWith("https://qa.buysellcycle.com/blog/post/"));
@@ -99,5 +102,93 @@ public class VisitorHomePageStepDef extends Base {
     public void close_the_page() {
         Driver.quitDriver();
     }
-}
 
+
+//====================steps Esra Baslangic================================
+
+    @Given("Displays the Track Your Order link on the top bar of the site")
+    public void displays_the_track_your_order_link_on_the_top_bar_of_the_site() {
+        assertTrue(visitorHomePage.linkTextTrackYourOrder.isDisplayed());
+    }
+
+    @Given("Displays the Compare link on the top bar of the site")
+    public void displays_the_compare_link_on_the_top_bar_of_the_site() {
+        assertTrue(visitorHomePage.linkTextCompare.isDisplayed());
+    }
+
+    @Given("Displays the Wish List link on the top bar of the site")
+    public void displays_the_wish_list_link_on_the_top_bar_of_the_site() {
+        assertTrue(visitorHomePage.linkTextWishList.isDisplayed());
+    }
+
+    @Given("Displays the Cart link on the top bar of the site")
+    public void displays_the_cart_link_on_the_top_bar_of_the_site() {
+        assertTrue(visitorHomePage.linkTextCart.isDisplayed());
+    }
+
+    @Given("Displays the New User Zone link on the top bar of the site")
+    public void displays_the_new_user_zone_link_on_the_top_bar_of_the_site() {
+        assertTrue(visitorHomePage.linkTextNewUserZone.isDisplayed());
+    }
+
+    @Given("Displays the Daily Deals link on the top bar of the site")
+    public void displays_the_daily_deals_link_on_the_top_bar_of_the_site() {
+        assertTrue(visitorHomePage.linkTextDailyDeals.isDisplayed());
+    }
+
+    @Given("Click on the Track your Order link on the top bar of the site and you will be redirected to the relevant page")
+    public void click_on_the_track_your_order_link_on_the_top_bar_of_the_site_and_you_will_be_redirected_to_the_relevant_page() {
+        clickWithJS(visitorHomePage.linkTextTrackYourOrder);
+        assertTrue(visitorHomePage.buttonTrackNow.isDisplayed());
+    }
+
+    @Given("Click on the Compare link on the top bar of the site and you will be redirected to the relevant page")
+    public void click_on_the_compare_link_on_the_top_bar_of_the_site_and_you_will_be_redirected_to_the_relevant_page() {
+        clickWithJS(visitorHomePage.linkTextCompare);
+        assertTrue(visitorHomePage.titleProductCompare.isDisplayed());
+    }
+
+    @Given("Click on the WishList link on the top bar of the site and you will be redirected to the relevant page")
+    public void click_on_the_wish_list_link_on_the_top_bar_of_the_site_and_you_will_be_redirected_to_the_relevant_page() {
+        clickWithJS(visitorHomePage.linkTextWishList);
+        assertTrue(visitorHomePage.titleSignIn.isDisplayed());
+    }
+
+    @Given("Click on the Cart link on the top bar of the site and you will be redirected to the relevant page")
+    public void click_on_the_cart_link_on_the_top_bar_of_the_site_and_you_will_be_redirected_to_the_relevant_page() {
+        clickWithJS(visitorHomePage.linkTextCart);
+        assertTrue(visitorHomePage.titleOrderSummary.isDisplayed());
+    }
+
+    @Given("Click on the New User Zone link on the top bar of the site and you will be redirected to the relevant page")
+    public void click_on_the_new_user_zone_link_on_the_top_bar_of_the_site_and_you_will_be_redirected_to_the_relevant_page() {
+        clickWithJS(visitorHomePage.linkTextNewUserZone);
+        assertTrue(visitorHomePage.titleSurpriseForNewUsers.isDisplayed());
+    }
+
+    @Given("Click on the Daily Deals link on the top bar of the site and you will be redirected to the relevant page")
+    public void click_on_the_daily_deals_link_on_the_top_bar_of_the_site_and_you_will_be_redirected_to_the_relevant_page() {
+        clickWithJS(visitorHomePage.linkTextDailyDeals);
+        assertTrue(visitorHomePage.titleDealEndsIn.isDisplayed());
+    }
+
+    @When("Return to Home page")
+    public void returnToHomePage() {
+        Driver.getDriver().navigate().back();
+    }
+
+    @Given("Clicks on the Site logo")
+    public void clicks_on_the_site_logo() {
+        clickWithJS(visitorHomePage.logoSite);
+    }
+
+    @Given("Refreshes the home page")
+    public void refreshes_the_home_page() {
+
+        String expectedTitle = "BuySellCycle | Multivendor Marketplace";
+        String actualTitle = Driver.getDriver().getTitle();
+        assertEquals(expectedTitle, actualTitle);
+    }
+
+
+}
