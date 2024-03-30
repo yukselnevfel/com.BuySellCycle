f71d09698be29499bfebde9e26b76bf08ce0411b
aslierk <asliekmekc@gmail.com>
2024-03-30 11:53:22 +0100

    duzeltme

--- src/test/java/pages/VisitorHomePage.java
+++ src/test/java/pages/VisitorHomePage.java
@@ -70,7 +70,7 @@ public class VisitorHomePage extends Base{
 
 
     @FindBy(xpath = "//*[@href='https://qa.buysellcycle.com/blog']")
-    public WebElement linkHeaderBlog1;
+    public WebElement linkHeaderBlog;
     @FindBy(xpath = "//*[@class='close_modal']")
     public WebElement cookieAllert;
     @FindBy(xpath = "//*[text()='+ Read more']")
