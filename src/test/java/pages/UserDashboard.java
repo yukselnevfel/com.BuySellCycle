4b83377a181677d688d4e4fd3cdc3438f55e39c3
fikretzeybek <fzeybek1972@gmail.com>
2024-03-30 13:20:10 +0300

    fikret/30.03/v2

--- src/test/java/pages/UserDashboard.java
+++ src/test/java/pages/UserDashboard.java
@@ -1,5 +1,28 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
 public class UserDashboard extends Base{
+
+   //Dashboard>> Purchase History page
+    @FindBy(xpath = "(//*[@class='position-relative d-flex align-items-center'])[1]")
+    public WebElement linkPurchaseHistory;
+
+    //Dashboard>> Purchase History link
+    @FindBy(css = ".font_20.f_w_700.mb-0 ")
+    public WebElement labelPurchaseHistory;
+
+    //Dashboard>> Purchase History Table
+    @FindBy (xpath = "//table//tr/th")
+    public List<WebElement> tablePurchaseHistory;
+
+    public void tableCellDisplayed(int number){
+        for (int i = 0; i < number; i++) {
+            tablePurchaseHistory.get(number).isDisplayed();
+        }
+    }
 
 }
--- src/test/java/stepdefinitions/VisitorHomePageStepDef.java
+++ src/test/java/stepdefinitions/VisitorHomePageStepDef.java
@@ -1,5 +1,8 @@
 package stepdefinitions;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.Keys;
 import pages.Base;
 import utils.Driver;
 import utils.ConfigReader;
@@ -44,24 +47,53 @@ public class VisitorHomePageStepDef extends Base {
 
     @Given("Verify that navigated to the Blog Page")
     public void verify_that_navigated_to_the_page() {
-        String expectedURL = "https://qa.buysellcycle.com/blog";
-        String actualURL = Driver.getDriver().getCurrentUrl();
-        assertEquals(expectedURL, actualURL);
+           assertEquals("https://qa.buysellcycle.com/blog", Driver.getDriver().getCurrentUrl());
     }
     @Given("Verify that navigated to the Read More Page")
     public void verify_that_navigated_to_the_read_more_page() {
-
+        clickWithJS(visitorHomePage.linkReadMore);
+        assertTrue(visitorHomePage.labelCategoryText.isDisplayed());
     }
 
     @Given("Enter a keyword into the search texBox and press Enter")
     public void enter_a_keyword_into_the_tex_box_and_press() {
-
+        visitorHomePage.blogSearchBox.sendKeys("metaverse "+ Keys.ENTER);
     }
 
     @Given("Verify that the search results are relevant to the entered keyword")
     public void verify_that_the_search_results_are_relevant_to_the_entered_keyword() {
+        assertTrue(visitorHomePage.labelMetaverseText.getText().contains("Metaverse"));
+    }
+    @When("Select a category from the Category section")
+    public void select_a_category_from_the_category_section() {
+        clickWithJS(visitorHomePage.selectCategoryPost);
+    }
+    @Then("Verify that blogs related to the selected category are listed")
+    public void verify_that_blogs_related_to_the_selected_category_are_listed() {
+        assertEquals("https://qa.buysellcycle.com/blog/category/posts/e-commerce-7", Driver.getDriver().getCurrentUrl());
+    }
+
+    @When("Select a keyword from the Keyword section")
+    public void select_a_keyword_from_the_keyword_section() {
+        visitorHomePage.blogKeyword.sendKeys("shopping");
+    }
+    @Then("Verify that relevant blogs are listed based on the selected keyword")
+    public void verify_that_relevant_blogs_are_listed_based_on_the_selected_keyword() {
+        String actualTagName = visitorHomePage.blogKeyword.getTagName();
+        String expectedTagName = "shopping";
+        assert actualTagName.equals(expectedTagName) : "Something went wrong";
+    }
+
+    @When("Select a category from the Popular Posts section")
+    public void select_a_category_from_the_popular_posts_section() {
+        clickWithJS(visitorHomePage.popularCategoryBlog);
+    }
+    @Then("Verify that the page with the selected blog post has been accessed")
+    public void verify_that_the_page_with_the_selected_blog_post_has_been_accessed() {
+        assertTrue(Driver.getDriver().getCurrentUrl().startsWith("https://qa.buysellcycle.com/blog/post/"));
 
     }
+
 
     @Given("Close the page")
     public void close_the_page() {
