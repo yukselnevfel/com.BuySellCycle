5338fb841287be742eecd90c583552ef47718376
Ayça Ovalı <145855586+OVALIAYCA@users.noreply.github.com>
2024-03-30 20:47:50 +0300

    main düzeltme

--- src/test/java/stepdefinitions/VisitorHomePageStepDef.java
+++ src/test/java/stepdefinitions/VisitorHomePageStepDef.java
@@ -1,6 +1,9 @@
 package stepdefinitions;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.Keys;
 import pages.Base;
 import utils.Driver;
 import utils.ConfigReader;
